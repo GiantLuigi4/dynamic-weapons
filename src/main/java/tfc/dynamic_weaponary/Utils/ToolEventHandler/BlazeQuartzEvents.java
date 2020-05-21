c4c635492cd038c50e66bd8e43fdf22b96bb1cd5
GiantLuigi4 <49770992+giantluigi4@users.noreply.github.com>
2020-05-20 20:05:49 -0400

    Apparently missed this.

--- src/main/java/tfc/dynamic_weaponary/Utils/ToolEventHandler/BlazeQuartzEvents.java
+++ src/main/java/tfc/dynamic_weaponary/Utils/ToolEventHandler/BlazeQuartzEvents.java
@@ -1,13 +1,9 @@
 package tfc.dynamic_weaponary.Utils.ToolEventHandler;
 
 import net.minecraft.entity.LivingEntity;
-import org.apache.logging.log4j.util.TriConsumer;
 
 public class BlazeQuartzEvents {
-	public static TriConsumer<LivingEntity, LivingEntity, Float> hitEvent = new TriConsumer<LivingEntity, LivingEntity, Float>() {
-		@Override
-		public void accept(LivingEntity livingEntity, LivingEntity playerEntity, Float aFloat) {
-			livingEntity.setFire((int) (120 * aFloat));
-		}
-	};
+	public void hitEntity(LivingEntity livingEntity, LivingEntity playerEntity, Float aFloat) {
+		livingEntity.setFire((int) (120 * aFloat));
+	}
 }
