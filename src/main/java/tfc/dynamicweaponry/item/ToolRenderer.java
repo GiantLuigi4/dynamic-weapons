1b5bf3a2bafa5bdb6da9ff64f424324d3afd3d87
GiantLuigi4 <49770992+GiantLuigi4@users.noreply.github.com>
2022-08-30 22:00:52 -0400

    normals, sword test

--- src/main/java/tfc/dynamicweaponry/item/ToolRenderer.java
+++ src/main/java/tfc/dynamicweaponry/item/ToolRenderer.java
@@ -2,6 +2,8 @@ package tfc.dynamicweaponry.item;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.math.Matrix3f;
+import com.mojang.math.Quaternion;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
@@ -20,6 +22,18 @@ public class ToolRenderer extends BlockEntityWithoutLevelRenderer {
 		return consumer.vertex(poseStack.last().pose(), (float) x, (float) y, (float) z);
 	}
 	
+	private static VertexConsumer normal(VertexConsumer consumer, ItemTransforms.TransformType pTransformType, PoseStack stack, float x, float y, float z) {
+		// TODO: what
+		if (pTransformType.equals(ItemTransforms.TransformType.GUI))
+			return consumer.normal(stack.last().normal(), 0, x, 0);
+		if (pTransformType.firstPerson())
+			return consumer.normal(x, y, z);
+		// TODO: figure this out
+		Matrix3f matr = stack.last().normal().copy();
+		matr.mul(new Quaternion(0, 90, 0, true));
+		return consumer.normal(stack.last().normal(), x, y, z);
+	}
+	
 	@Override
 	public void renderByItem(ItemStack pStack, ItemTransforms.TransformType pTransformType, PoseStack pPoseStack, MultiBufferSource pBuffer, int pPackedLight, int pPackedOverlay) {
 		ToolImage image = Temp.image;
@@ -33,40 +47,40 @@ public class ToolRenderer extends BlockEntityWithoutLevelRenderer {
 			
 			if (x < 16) {
 				// far end
-				vertex(consumer, pPoseStack, pctX, 0, 0.5 - step / 2).color(255, 255, 255, 255).uv(pctX, 1).overlayCoords(pPackedOverlay).uv2(pPackedLight).normal(0, 0, -1).endVertex();
-				vertex(consumer, pPoseStack, pctX, 0, 0.5 + step / 2).color(255, 255, 255, 255).uv(pctX + step, 1).overlayCoords(pPackedOverlay).uv2(pPackedLight).normal(0, 0, -1).endVertex();
-				vertex(consumer, pPoseStack, pctX, 1, 0.5 + step / 2).color(255, 255, 255, 255).uv(pctX + step, 0).overlayCoords(pPackedOverlay).uv2(pPackedLight).normal(0, 0, -1).endVertex();
-				vertex(consumer, pPoseStack, pctX, 1, 0.5 - step / 2).color(255, 255, 255, 255).uv(pctX, 0).overlayCoords(pPackedOverlay).uv2(pPackedLight).normal(0, 0, -1).endVertex();
+				normal(vertex(consumer, pPoseStack, pctX, 0, 0.5 - step / 2).color(255, 255, 255, 255).uv(pctX, 1).overlayCoords(pPackedOverlay).uv2(pPackedLight), pTransformType, pPoseStack, 0, 0, -1).endVertex();
+				normal(vertex(consumer, pPoseStack, pctX, 0, 0.5 + step / 2).color(255, 255, 255, 255).uv(pctX + step, 1).overlayCoords(pPackedOverlay).uv2(pPackedLight), pTransformType, pPoseStack, 0, 0, -1).endVertex();
+				normal(vertex(consumer, pPoseStack, pctX, 1, 0.5 + step / 2).color(255, 255, 255, 255).uv(pctX + step, 0).overlayCoords(pPackedOverlay).uv2(pPackedLight), pTransformType, pPoseStack, 0, 0, -1).endVertex();
+				normal(vertex(consumer, pPoseStack, pctX, 1, 0.5 - step / 2).color(255, 255, 255, 255).uv(pctX, 0).overlayCoords(pPackedOverlay).uv2(pPackedLight), pTransformType, pPoseStack, 0, 0, -1).endVertex();
 				
 				// bottom
-				vertex(consumer, pPoseStack, 0, pctX, 0.5 + step / 2).color(255, 255, 255, 255).uv(0, 1 - pctX).overlayCoords(pPackedOverlay).uv2(pPackedLight).normal(0, -1, 0).endVertex();
-				vertex(consumer, pPoseStack, 0, pctX, 0.5 - step / 2).color(255, 255, 255, 255).uv(0, 1 - (pctX + step)).overlayCoords(pPackedOverlay).uv2(pPackedLight).normal(0, -1, 0).endVertex();
-				vertex(consumer, pPoseStack, 1, pctX, 0.5 - step / 2).color(255, 255, 255, 255).uv(1, 1 - (pctX + step)).overlayCoords(pPackedOverlay).uv2(pPackedLight).normal(0, -1, 0).endVertex();
-				vertex(consumer, pPoseStack, 1, pctX, 0.5 + step / 2).color(255, 255, 255, 255).uv(1, 1 - pctX).overlayCoords(pPackedOverlay).uv2(pPackedLight).normal(0, -1, 0).endVertex();
+				normal(vertex(consumer, pPoseStack, 0, pctX, 0.5 + step / 2).color(255, 255, 255, 255).uv(0, 1 - pctX).overlayCoords(pPackedOverlay).uv2(pPackedLight), pTransformType, pPoseStack, 0, -1, 0).endVertex();
+				normal(vertex(consumer, pPoseStack, 0, pctX, 0.5 - step / 2).color(255, 255, 255, 255).uv(0, 1 - (pctX + step)).overlayCoords(pPackedOverlay).uv2(pPackedLight), pTransformType, pPoseStack, 0, -1, 0).endVertex();
+				normal(vertex(consumer, pPoseStack, 1, pctX, 0.5 - step / 2).color(255, 255, 255, 255).uv(1, 1 - (pctX + step)).overlayCoords(pPackedOverlay).uv2(pPackedLight), pTransformType, pPoseStack, 0, -1, 0).endVertex();
+				normal(vertex(consumer, pPoseStack, 1, pctX, 0.5 + step / 2).color(255, 255, 255, 255).uv(1, 1 - pctX).overlayCoords(pPackedOverlay).uv2(pPackedLight), pTransformType, pPoseStack, 0, -1, 0).endVertex();
 			}
 			if (x > 0) {
 				// near end
-				vertex(consumer, pPoseStack, pctX, 0, 0.5 + step / 2).color(255, 255, 255, 255).uv(pctX, 1).overlayCoords(pPackedOverlay).uv2(pPackedLight).normal(0, 0, 1).endVertex();
-				vertex(consumer, pPoseStack, pctX, 0, 0.5 - step / 2).color(255, 255, 255, 255).uv(pctX - step, 1).overlayCoords(pPackedOverlay).uv2(pPackedLight).normal(0, 0, 1).endVertex();
-				vertex(consumer, pPoseStack, pctX, 1, 0.5 - step / 2).color(255, 255, 255, 255).uv(pctX - step, 0).overlayCoords(pPackedOverlay).uv2(pPackedLight).normal(0, 0, 1).endVertex();
-				vertex(consumer, pPoseStack, pctX, 1, 0.5 + step / 2).color(255, 255, 255, 255).uv(pctX, 0).overlayCoords(pPackedOverlay).uv2(pPackedLight).normal(0, 0, 1).endVertex();
+				normal(vertex(consumer, pPoseStack, pctX, 0, 0.5 + step / 2).color(255, 255, 255, 255).uv(pctX, 1).overlayCoords(pPackedOverlay).uv2(pPackedLight), pTransformType, pPoseStack, 0, 0, 1).endVertex();
+				normal(vertex(consumer, pPoseStack, pctX, 0, 0.5 - step / 2).color(255, 255, 255, 255).uv(pctX - step, 1).overlayCoords(pPackedOverlay).uv2(pPackedLight), pTransformType, pPoseStack, 0, 0, 1).endVertex();
+				normal(vertex(consumer, pPoseStack, pctX, 1, 0.5 - step / 2).color(255, 255, 255, 255).uv(pctX - step, 0).overlayCoords(pPackedOverlay).uv2(pPackedLight), pTransformType, pPoseStack, 0, 0, 1).endVertex();
+				normal(vertex(consumer, pPoseStack, pctX, 1, 0.5 + step / 2).color(255, 255, 255, 255).uv(pctX, 0).overlayCoords(pPackedOverlay).uv2(pPackedLight), pTransformType, pPoseStack, 0, 0, 1).endVertex();
 				
 				// top
-				vertex(consumer, pPoseStack, 0, pctX, 0.5 - step / 2).color(255, 255, 255, 255).uv(0, 1 - pctX).overlayCoords(pPackedOverlay).uv2(pPackedLight).normal(0, 1, 0).endVertex();
-				vertex(consumer, pPoseStack, 0, pctX, 0.5 + step / 2).color(255, 255, 255, 255).uv(0, 1 - (pctX - step)).overlayCoords(pPackedOverlay).uv2(pPackedLight).normal(0, 1, 0).endVertex();
-				vertex(consumer, pPoseStack, 1, pctX, 0.5 + step / 2).color(255, 255, 255, 255).uv(1, 1 - (pctX - step)).overlayCoords(pPackedOverlay).uv2(pPackedLight).normal(0, 1, 0).endVertex();
-				vertex(consumer, pPoseStack, 1, pctX, 0.5 - step / 2).color(255, 255, 255, 255).uv(1, 1 - pctX).overlayCoords(pPackedOverlay).uv2(pPackedLight).normal(0, 1, 0).endVertex();
+				normal(vertex(consumer, pPoseStack, 0, pctX, 0.5 - step / 2).color(255, 255, 255, 255).uv(0, 1 - pctX).overlayCoords(pPackedOverlay).uv2(pPackedLight), pTransformType, pPoseStack, 0, 1, 0).endVertex();
+				normal(vertex(consumer, pPoseStack, 0, pctX, 0.5 + step / 2).color(255, 255, 255, 255).uv(0, 1 - (pctX - step)).overlayCoords(pPackedOverlay).uv2(pPackedLight), pTransformType, pPoseStack, 0, 1, 0).endVertex();
+				normal(vertex(consumer, pPoseStack, 1, pctX, 0.5 + step / 2).color(255, 255, 255, 255).uv(1, 1 - (pctX - step)).overlayCoords(pPackedOverlay).uv2(pPackedLight), pTransformType, pPoseStack, 0, 1, 0).endVertex();
+				normal(vertex(consumer, pPoseStack, 1, pctX, 0.5 - step / 2).color(255, 255, 255, 255).uv(1, 1 - pctX).overlayCoords(pPackedOverlay).uv2(pPackedLight), pTransformType, pPoseStack, 0, 1, 0).endVertex();
 			}
 		}
 		
-		vertex(consumer, pPoseStack, 0, 0, 0.5 + step / 2).color(255, 255, 255, 255).uv(0, 1).overlayCoords(pPackedOverlay).uv2(pPackedLight).normal(1, 0, 0).endVertex();
-		vertex(consumer, pPoseStack, 1, 0, 0.5 + step / 2).color(255, 255, 255, 255).uv(1, 1).overlayCoords(pPackedOverlay).uv2(pPackedLight).normal(1, 0, 0).endVertex();
-		vertex(consumer, pPoseStack, 1, 1, 0.5 + step / 2).color(255, 255, 255, 255).uv(1, 0).overlayCoords(pPackedOverlay).uv2(pPackedLight).normal(1, 0, 0).endVertex();
-		vertex(consumer, pPoseStack, 0, 1, 0.5 + step / 2).color(255, 255, 255, 255).uv(0, 0).overlayCoords(pPackedOverlay).uv2(pPackedLight).normal(1, 0, 0).endVertex();
+		normal(vertex(consumer, pPoseStack, 0, 0, 0.5 + step / 2).color(255, 255, 255, 255).uv(0, 1).overlayCoords(pPackedOverlay).uv2(pPackedLight), pTransformType, pPoseStack, 1, 0, 0).endVertex();
+		normal(vertex(consumer, pPoseStack, 1, 0, 0.5 + step / 2).color(255, 255, 255, 255).uv(1, 1).overlayCoords(pPackedOverlay).uv2(pPackedLight), pTransformType, pPoseStack, 1, 0, 0).endVertex();
+		normal(vertex(consumer, pPoseStack, 1, 1, 0.5 + step / 2).color(255, 255, 255, 255).uv(1, 0).overlayCoords(pPackedOverlay).uv2(pPackedLight), pTransformType, pPoseStack, 1, 0, 0).endVertex();
+		normal(vertex(consumer, pPoseStack, 0, 1, 0.5 + step / 2).color(255, 255, 255, 255).uv(0, 0).overlayCoords(pPackedOverlay).uv2(pPackedLight), pTransformType, pPoseStack, 1, 0, 0).endVertex();
 		
-		vertex(consumer, pPoseStack, 0, 1, 0.5 - step / 2).color(255, 255, 255, 255).uv(0, 0).overlayCoords(pPackedOverlay).uv2(pPackedLight).normal(-1, 0, 0).endVertex();
-		vertex(consumer, pPoseStack, 1, 1, 0.5 - step / 2).color(255, 255, 255, 255).uv(1, 0).overlayCoords(pPackedOverlay).uv2(pPackedLight).normal(-1, 0, 0).endVertex();
-		vertex(consumer, pPoseStack, 1, 0, 0.5 - step / 2).color(255, 255, 255, 255).uv(1, 1).overlayCoords(pPackedOverlay).uv2(pPackedLight).normal(-1, 0, 0).endVertex();
-		vertex(consumer, pPoseStack, 0, 0, 0.5 - step / 2).color(255, 255, 255, 255).uv(0, 1).overlayCoords(pPackedOverlay).uv2(pPackedLight).normal(-1, 0, 0).endVertex();
+		normal(vertex(consumer, pPoseStack, 0, 1, 0.5 - step / 2).color(255, 255, 255, 255).uv(0, 0).overlayCoords(pPackedOverlay).uv2(pPackedLight), pTransformType, pPoseStack, -1, 0, 0).endVertex();
+		normal(vertex(consumer, pPoseStack, 1, 1, 0.5 - step / 2).color(255, 255, 255, 255).uv(1, 0).overlayCoords(pPackedOverlay).uv2(pPackedLight), pTransformType, pPoseStack, -1, 0, 0).endVertex();
+		normal(vertex(consumer, pPoseStack, 1, 0, 0.5 - step / 2).color(255, 255, 255, 255).uv(1, 1).overlayCoords(pPackedOverlay).uv2(pPackedLight), pTransformType, pPoseStack, -1, 0, 0).endVertex();
+		normal(vertex(consumer, pPoseStack, 0, 0, 0.5 - step / 2).color(255, 255, 255, 255).uv(0, 1).overlayCoords(pPackedOverlay).uv2(pPackedLight), pTransformType, pPoseStack, -1, 0, 0).endVertex();
 	}
 }
